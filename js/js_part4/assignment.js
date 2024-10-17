e21c44bb849801e3f4b57400efada4852c88de9a
codal-tshah <tshah@codal.com>
2024-10-17 11:37:09 +0530

    CIT-539 #time 2h Compledted the Javascript learning Part 4

---
+++ js/js_part4/assignment.js
@@ -0,0 +1,14 @@
+//Question 1 
+let arr = [1,2,3,4,5,6,2,3]
+let num = 2
+let i = 0
+while(i<arr.length){
+    if(arr[i]==num){
+        arr.splice(i,1);
+    }
+    i++;
+}
+console.log(arr)
+
+//Question 2 
+let num1 = 6642120
